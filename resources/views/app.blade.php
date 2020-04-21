fbed94c2def08465aa90495098e9e169fe358ed9
IT-MiraclePark <it.miraclepark@gmail.com>
2020-04-21 15:15:07 +0700

    reset

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,99 +7,40 @@
 
 	@yield('aimeos_header')
 
-	<title>Mlijo Indonesia</title>
+	<title>Mlijo</title>
 
 	@yield('aimeos_styles')
 
-	<link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
-
-	<link rel="stylesheet" href="<?php echo asset('themes/vagefoods/css/open-iconic-bootstrap.min.css'); ?>">
-    <link rel="stylesheet" href="<?php echo asset('themes/vagefoods/css/animate.css'); ?>">
-    
-    <link rel="stylesheet" href="<?php echo asset('themes/vagefoods/css/owl.carousel.min.css'); ?>">
-    <link rel="stylesheet" href="<?php echo asset('themes/vagefoods/css/owl.theme.default.min.css'); ?>">
-    <link rel="stylesheet" href="<?php echo asset('themes/vagefoods/css/magnific-popup.css'); ?>">
-
-    <link rel="stylesheet" href="<?php echo asset('themes/vagefoods/css/aos.css'); ?>">
-
-    <link rel="stylesheet" href="<?php echo asset('themes/vagefoods/css/ionicons.min.css'); ?>">
-
-    <link rel="stylesheet" href="<?php echo asset('themes/vagefoods/css/bootstrap-datepicker.css'); ?>">
-    <link rel="stylesheet" href="<?php echo asset('themes/vagefoods/css/jquery.timepicker.css'); ?>">
-
-    
-    <link rel="stylesheet" href="<?php echo asset('themes/vagefoods/css/flaticon.css'); ?>">
-    <link rel="stylesheet" href="<?php echo asset('themes/vagefoods/css/icomoon.css'); ?>">
-    <link rel="stylesheet" href="<?php echo asset('themes/vagefoods/css/style.css'); ?>">
+	<link type="text/css" rel="stylesheet" href='https://fonts.googleapis.com/css?family=Roboto:400,300'>
+	<link type="text/css" rel="stylesheet" href="/css/app.css">
 
 </head>
-<body class="goto-here">
-	<!--small header-->
-	<div class="py-1 bg-primary d-none d-md-block">
-    	<div class="container">
-    		<div class="row no-gutters d-flex align-items-start align-items-center px-md-0">
-	    		<div class="col-lg-12 d-block">
-		    		<div class="row d-flex">
-		    			<div class="col-md pr-4 d-flex topper align-items-center">
-					    	<div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-phone2"></span></div>
-						    <span class="text">0859-0377-8000</span>
-					    </div>
-					    <div class="col-md pr-4 d-flex topper align-items-center">
-					    	<div class="icon mr-2 d-flex justify-content-center align-items-center"><span class="icon-paper-plane"></span></div>
-						    <span class="text">info@mlijo.id</span>
-					    </div>
-					    <div class="col-md-5 pr-4 d-flex topper align-items-center text-lg-right">
-						    <span class="text">Penuhi Kebutuhan Sayur Segar Anda Disini!</span>
-					    </div>
-				    </div>
-			    </div>
-		    </div>
-		  </div>
-	</div>
-	<!--end small header-->
-	<!--nav-->
-	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-	    <div class="container">
-			<a class="navbar-brand" href="/">
-				<img src="<?php echo asset('files/logo.png');?>" height="30" title="Mlijo Logo">
-			</a>
-	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-	        <span class="oi oi-menu"></span> Menu
-	      </button>
-
-	      <div class="collapse navbar-collapse" id="ftco-nav">
-	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item active"><a href="/" class="nav-link">Home</a></li>
-	          <li class="nav-item"><a href="/about" class="nav-link">About</a></li>
-	          <li class="nav-item"><a href="/contact" class="nav-link">Contact</a></li>
-			  <li class="nav-item cta cta-colored"><a href="#" class="nav-link"><span class="icon-shopping_cart"></span>[0]</a></li>
-			  
-			  @if(Auth::guest())
-			  <li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Akun</a>
-				<div class="dropdown-menu" aria-labelledby="dropdown04">
-				  <a class="dropdown-item" href="/login">Login</a>
-				  <a class="dropdown-item" href="/register">Register</a>
-				</div>
-			  </li>
-			  @else
-			  <li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
-				<div class="dropdown-menu" aria-labelledby="dropdown04">
-					<a class="dropdown-item" href="{{ route('aimeos_shop_account',['site'=>Route::current()->parameter('site','default'),'locale'=>Route::current()->parameter('locale','en'),'currency'=>Route::current()->parameter('currency','EUR')]) }}" title="Profile">Profile</a>
-					<form id="logout" action="/logout" method="POST">{{csrf_field()}}</form><a class="dropdown-item" href="javascript: document.getElementById('logout').submit();">Logout</a>
-				</div>
-			  </li>
-			  @endif
-			  @yield('aimeos_head')
+<body>
+	<nav class="navbar navbar-expand-sm navbar-light">
+		<a class="navbar-brand" href="/">
+			<img src="http://aimeos.org/fileadmin/template/icons/logo.png" height="30" title="Mlijo Logo">
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				@if (Auth::guest())
+					<li class="nav-item navbar-text"><a class="nav-link" href="/login">Login</a></li>
+					<li class="nav-item navbar-text"><a class="nav-link" href="/register">Register</a></li>
+				@else
+					<li class="nav-item navbar-text dropdown">
+						<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="{{ route('aimeos_shop_account',['site'=>Route::current()->parameter('site','default'),'locale'=>Route::current()->parameter('locale','en'),'currency'=>Route::current()->parameter('currency','EUR')]) }}" title="Profile">Profile</a></li>
+							<li><form id="logout" action="/logout" method="POST">{{csrf_field()}}</form><a href="javascript: document.getElementById('logout').submit();">Logout</a></li>
+						</ul>
+					</li>
+				@endif
 			</ul>
-	      </div>
-	    </div>
-	  </nav>
-	<!--end nav-->
-	  
+			@yield('aimeos_head')
+		</div>
+	</nav>
     <div class="container">
 		@yield('aimeos_nav')
 		@yield('aimeos_stage')
